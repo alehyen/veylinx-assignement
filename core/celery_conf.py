754896335317e179db121d3bfa58b769c542ed1e
Ismail <ismailalehyen@gmail.com>
2021-05-08 23:27:16 +0000

    dockerize the app

--- core/celery_conf.py
+++ core/celery_conf.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('core')
+app = Celery('core.celery_conf')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
